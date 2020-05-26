0184801d2b2fcda1b99d5e35f1d03eabd542881e
Lujine <lujine_2005@yahoo.com>
2020-05-26 14:24:49 -0400

    i had to push this

---
+++ background.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class background extends Application {
+    
+}
