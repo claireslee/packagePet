b4ac6ddd95567777fe03ec886a0af11697a12e70
Claire <clairebearlee0126@gmail.com>
2020-05-27 14:37:02 -0400

    new type of buttons

--- Bar.java
+++ Bar.java
@@ -4,29 +4,30 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
 
 public class Bar extends Application {
 	private TextField tfNumber1 = new TextField();
 	private TextField tfNumber2 = new TextField();
     private TextField tfResult = new TextField();
-    
-	private Button btFood = new Button("Food");
-	private Button btShower = new Button("Shower");
-	private Button btDeath = new Button("Death");
-    private Button btClothes = new Button("Clothes");
-    private Button btBall = new Button("Ball");
 
 	@Override 
 	public void start(Stage primaryStage) {
-		HBox hBox1 = new HBox(5);
-		HBox hBox2 = new HBox(5);
-		VBox vBox = new VBox(15);
-		hBox1.getChildren().addAll(btFood, btShower, btDeath, btClothes, btBall);
-		vBox.getChildren().addAll(hBox1, hBox2);
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(new Group());
+		primaryStage.setTitle("Package Pet");
+
+		HBox hBox1 = new HBox(10);
+		HBox hBox2 = new HBox(10);
+		VBox vBox = new VBox(20);
 
 		hBox1.setAlignment(Pos.CENTER);
 		hBox2.setAlignment(Pos.CENTER);
@@ -42,14 +43,61 @@ public class Bar extends Application {
 		// btDeath.setOnAction(e -> death());
 		// btClothes.setOnAction(e -> clothes());
 		// btBall.setOnAction(e -> ball());
+	
+	// buttons as images
+	Image image1 = new Image(getClass().getResourceAsStream("images/foodicon.png"));
+        Button food = new Button("food");
+        food.setGraphic(new ImageView(image1));
+        food.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                //
+            }
+        });
+	
+	Image image2 = new Image(getClass().getResourceAsStream("images/clothesicon.png"));
+		Button clothes = new Button("clothes");
+        food.setGraphic(new ImageView(image2));
+        food.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Accepted");
+            }
+		});
 
-		// Create a scene and place it in the stage
-		Scene scene = new Scene(vBox);
-		primaryStage.setTitle("Package Pet");
+	Image image3 = new Image(getClass().getResourceAsStream("images/balicon.png"));
+		Button ball = new Button("ball");
+        food.setGraphic(new ImageView(image3));
+        food.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Accepted");
+            }
+		});
+		
+	Image image4 = new Image(getClass().getResourceAsStream("images/showericon.png"));
+		Button shower = new Button("shower");
+        food.setGraphic(new ImageView(image4));
+        food.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Accepted");
+            }
+		});
+
+	Image image5 = new Image(getClass().getResourceAsStream("images/deathicon.png"));	
+		Button death = new Button("death");
+        food.setGraphic(new ImageView(image5));
+        food.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Accepted");
+            }
+		});
+		
+		hBox1.getChildren().addAll(food);
+		vBox.getChildren().addAll(hBox1, hBox2);
+		vBox.getChildren().addAll(food, clothes, ball, shower, death);
+		((Group)scene.getRoot()).getChildren().add(vBox);
+
 		primaryStage.setScene(scene); 
 		primaryStage.show(); 
 	}
-
 
 // 	// Button Functions
 // 	private void food() {
@@ -71,6 +119,9 @@ public class Bar extends Application {
 // 	private void ball() {
 // 		return 0;
 // 	}
+
+	// public static void main(String[] args) {
+	// 	launch(args);
+	// }
+
 }
-
-
