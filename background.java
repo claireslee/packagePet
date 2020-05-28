ed77854df924f7e74997421132e0c929a8a4db7c
dsilva-rhianna <rhiannadsilva@gmail.com>
2020-05-28 14:19:59 -0400

    bgbinding

--- background.java
+++ background.java
@@ -1,10 +1,8 @@
 import javafx.application.Application;
-//import javafx.scene.Group;
+import javafx.scene.layout.Pane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import javafx.scene.layout.StackPane;
-//import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,12 +10,23 @@ public class background extends Application {
     @Override
     public void start (Stage primaryStage){
 
-        GridPane pane = new GridPane();
-        pane.add(new ImageView(new Image("images/backgroundpic.jpg")), 0, 0);
+        Image bg = new Image("images/backgroundpic.jpg");
+        ImageView park = new ImageView();
+        park.setImage(bg);
 
+        Pane pane = new Pane();
         Scene scene = new Scene(pane);
-		primaryStage.setTitle("Package Pet"); 
-		primaryStage.setScene(scene); 
-		primaryStage.show();
+        HBox box = new HBox();
+        box.getChildren().add(park);
+        pane.getChildren().add(box);
+        pane.setMinSize(0, 0);
+        park.fitWidthProperty().bind(pane.widthProperty());
+        park.fitHeightProperty().bind(pane.heightProperty());
+		primaryStage.setTitle("Package Pet");
+		primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
