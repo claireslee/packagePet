076d0c502a76a4b0f858e6771a3d446589162a2f
Maggie <margaret.anne.schneider@gmail.com>
2020-05-29 14:28:22 -0400

    powerbar update

--- powerBar.java
+++ powerBar.java
@@ -1,3 +1,9 @@
+import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,6 +15,7 @@ import javafx.scene.text.Text;
 import javafx.scene.shape.Rectangle;
 
 public class powerBar extends Application {
+    double x = 70;
     @Override 
     public void start(Stage primaryStage) {
     GridPane pane = new GridPane();
@@ -17,28 +24,15 @@ public class powerBar extends Application {
     r1.setStroke(Color.BLACK);
     r1.setFill(Color.WHITE);
 
-    double x = 70;
     Rectangle r2 = new Rectangle(40, 70, x, 40);
     r2.setStroke(Color.BLACK);
     r2.setFill(Color.BLACK);
-
-    long mTime = System.currentTimeMillis();
-    long end = mTime + 5000; // 5 seconds 
-    while (System.currentTimeMillis() > end) {
-        if (x==10){
-            //kill it
-        }
-        else{
-            x-=20;
-            pane.getChildren().remove(r2);
-            Rectangle r3 = new Rectangle(40, 70, x, 40);
-            r3.setStroke(Color.BLACK);
-            r3.setFill(Color.BLACK);
-
-        }
     
-    }
-    pane.getChildren().addAll(r1, r2);
+        
+    
+    
+
+    pane.getChildren().addAll(r1);
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane);
     primaryStage.setTitle("powerBar"); // Set the stage title
