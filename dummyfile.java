6b961a69d7b964004bc8ad07a95e8660ecdfd0cf
Maggie <margaret.anne.schneider@gmail.com>
2020-06-03 14:29:59 -0400

    powerbar

--- dummyfile.java
+++ dummyfile.java
@@ -8,35 +8,49 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 public class dummyfile extends Application {
+    double x = 70;
 @Override // Override the start method in the Application class
 public void start(Stage primaryStage) {
 StackPane pane = new StackPane();
-Text text = new Text(20, 50, "Programming is fun");
-text.setFill(Color.RED);
-pane.getChildren().add(text); // Place text into the stack pane
+ // Place text into the stack pane
+
+Rectangle r1 = new Rectangle(40, x);
+r1.setStroke(Color.BLACK);
+r1.setFill(Color.BLACK);
+
+
+Rectangle r3 = new Rectangle(40, 70);
+r3.setStroke(Color.BLACK);
+r3.setFill(Color.WHITE);
+
+pane.getChildren().addAll(r3, r1);
 
 // Create a handler for changing text
 EventHandler<ActionEvent> eventHandler = e -> {
-if (text.getText().length() != 0) {
-text.setText("");
-}
+if (r1.getHeight() != 0) {
+x-=10;
+r1.setHeight(x);
+} 
 else {
-text.setText("Programming is fun");
-}
+ 
+x-=10;
+r1.setHeight(x);
+} //15
 };
 
 // Create an animation for alternating text
 Timeline animation = new Timeline(
-new KeyFrame(Duration.millis(500), eventHandler));
+new KeyFrame(Duration.millis(5000), eventHandler));
 animation.setCycleCount(Timeline.INDEFINITE);
 animation.play(); // Start animation
 
 // Pause and resume animation
-text.setOnMouseClicked(e -> {
+pane.setOnMouseClicked(e -> {
 if (animation.getStatus() == Animation.Status.PAUSED) {
 animation.play();
 }
